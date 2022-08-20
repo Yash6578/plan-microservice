1b53fdd59ae5611e5fa71f90cf671b12a88ad8c9
yash6578 <yashchawla6578@gmail.com>
2022-08-20 14:14:28 +0530

    Initial commit

---
+++ src/main/java/in/ashokit/planmicroservice/PlanMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit.planmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanMicroserviceApplication.class, args);
+    }
+
+}
